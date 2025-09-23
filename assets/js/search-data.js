dfd49595735b1d65b60a58be697ca0a00bb47d3d
vgandhi13 <82404434+vgandhi13@users.noreply.github.com>
2025-09-23 01:17:35 +0000

    Deploying to gh-pages from @ vgandhi13/vgandhi13.github.io@8311ea2bb34a0f9d910bc6f055aeb87529c6bdf3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -394,15 +394,15 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-awarded-bay-state-fellowship-by-manning-cics",
+          title: 'Awarded Bay State Fellowship by Manning CICS',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
